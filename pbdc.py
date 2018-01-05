118b6ddc9c80ac2758f5c3d9b81841fc91fc32f9
hex <hex@hex.gs.com>
2018-01-05 11:49:32 +0800

    add intit files

---
+++ pbdc.py
@@ -0,0 +1,167 @@
+#!/bin/python
+#-*- coding:utf-8 -*-
+from __future__ import unicode_literals
+from jinja2 import Template, Environment
+import os
+
+
+def camel_style_trans(cname):
+    pre_is_lower = False
+    rname = ''
+    for ch in cname:
+        if ch.isupper():
+            if pre_is_lower:
+                rname += '_'
+                pre_is_lower = False
+            rname += ch.lower()
+        else:
+            rname += ch 
+            pre_is_lower = True
+    return rname
+
+
+#CSMsgLoginQuery->LoginQuery->login_query
+def rpc_name(entry_type_name, service_name):
+    pos = entry_type_name.find(service_name)
+    if pos == -1:
+        raise Exception('rpc define error for pdE type do not have a service type prefix type:%s' % entry_type_name)
+    return camel_style_trans(entry_type_name[pos+len(service_name):])
+
+#CSMsgLoginQuery->LOGIN_QUERY
+def rpc_cmd(entry_type_name, service_name, cmd_prefix):
+    #print cmd_prefix
+    rname = rpc_name(entry_type_name, service_name)
+    return cmd_prefix + rname.upper()
+    
+tpenv = Environment()
+tpenv.filters={'rpc_name':rpc_name,'rpc_cmd':rpc_cmd}
+
+class DefStage(dict):
+    def __init__(self, type, name, options={}):
+        self.type = type
+        self.name = name
+        self.options = options
+        self.fields = []
+        self.req = []
+        self.res = []
+
+    def __getattr__(self, attr):
+        return self.get(attr, None)
+
+    def __setattr__(self, attr, val):
+        self[attr]=val
+
+
+class Ctx(dict):
+    def __init__(self):
+        self.reset()
+
+    def reset(self, name=''):
+        self.file = name
+        self.imports = []
+        self.package = ''
+        self.stack = []
+        self.meta = {}
+        self.defs = []
+
+    def __getattr__(self, attr):
+        return self.get(attr, None)
+
+    def __setattr__(self, attr, val):
+        self[attr]=val
+
+
+########################################################################################################
+class CodeGenerater(object):
+    def __init__(self, ext_name, template):
+        self.ext_name=ext_name
+        self.template=template
+########################################################################################################
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),'templates')
+backends = {
+    'pb': CodeGenerater('.proto','pb2.proto')
+}
+########################################################################################################
+gtx = Ctx()
+def pdFile(name):
+    gtx.reset(name)
+
+def pdImport(name):
+    gtx.imports.append(name)
+
+def pdPackage(name):
+    gtx.package = name
+
+def pdMsg(name, **kwargs):
+    gtx.stack.append(DefStage('msg', name, kwargs))
+
+def pdService(name, **kwargs):
+    gtx.stack.append(DefStage('service', name, kwargs))
+
+def pdEnum(name, **kwargs):
+    #print gtx.stack
+    gtx.stack.append(DefStage('enum', name, kwargs))
+
+def pdTab(name, **kwargs):
+    gtx.stack.append(DefStage('res', name, kwargs))
+
+def pdRpc(name, **kwargs):
+    gtx.stack.append(DefStage('rpc', name, kwargs))
+
+
+def check_keys(kwargs, keys=[]):
+    for k in keys:
+        v = kwargs.get(k, None)
+        if v is None:
+            raise Exception('not found the key ' % str(keys))
+
+
+def pdRes(**kwargs):
+    if 'rpc' != gtx.stack[-1].type:
+        raise Exception('def response should be in a Rpc context (need a pdRpc before)')
+    check_keys(kwargs, ['t','n'])
+    gtx.stack[-1].res.append(kwargs)
+
+def pdReq(**kwargs):
+    if 'rpc' != gtx.stack[-1].type:
+        raise Exception('def request should be in a Rpc context (need a pdRpc before)')
+    check_keys(kwargs, ['t','n'])
+    gtx.stack[-1].req.append(kwargs)
+
+def pdE(t,**kwargs):
+    assert len(gtx.stack) > 0,'define field (entry) not in a context'
+    kwargs['t'] = t
+    if gtx.stack[-1].type == 'msg':
+    	check_keys(kwargs, ['n','t'])
+    gtx.stack[-1].fields.append(kwargs)
+
+def pdA(t, **kwargs):
+    assert len(gtx.stack) > 0,'define field (entry) not in a context'
+    kwargs['repeat'] = True
+    pdE(**kwargs)
+
+
+def pdGenerate(ctx, codegen, outdir):
+    for cg in codegen:
+        assert cg in backends, 'not found the codgerator for:'+cg
+        tp = os.path.join(template_dir,backends[cg].template)
+        ext_name = backends[cg].ext_name
+        path = os.path.join(outdir, ctx.file+ext_name)
+        try:
+            data = tpenv.from_string(open(tp).read()).render(ctx).encode('utf-8')
+        except Exception,e:
+            raise Exception('syntax error for proto def for "%s" code gen type:%s' % (str(e), cg))
+        open(path,'wb+').write(data)
+
+
+def pdEnd(codegen=['pb'], outdir='.'):
+    assert len(gtx.stack) >= 0,'defination not match for end'
+    if len(gtx.stack) == 0:
+        #print gtx
+        pdGenerate(gtx, codegen, outdir)
+        gtx.reset()
+    else:
+        ddef = gtx.stack.pop()
+        gtx.meta[ddef.name] = ddef
+        gtx.defs.append(ddef)
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+setup(  
+      name='pbdc',
+      version='0.1.0',
+      description="python describe data structure and represent with many tools like C/C++ syntax , proto syntax, json, ...etc",
+      keywords='protobuf c++ c memory flat python',  
+      author='jj4jj',
+      author_email='resc@vip.qq.com',
+      license='MIT License',
+      py_modules=['pbdc'],
+      packages = find_packages('./'),
+      requires = ['Jinja2'],
+      include_package_data = True,
+      data_files = ['templates/pb2.proto'],
+      url='https://github.com/jj4jj/pbdc',
+)
+
