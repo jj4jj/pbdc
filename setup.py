52b03bd37a869fed708941588f33e6ddd3cc3842
hex <hex@hex.gs.com>
2018-01-05 13:34:49 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup,find_packages
 setup(  
       name='pbdc',
-      version='0.1.0',
+      version='0.1.1',
       description="python describe data structure and represent with many tools like C/C++ syntax , proto syntax, json, ...etc",
-      keywords='protobuf c++ c memory flat python',  
+      keywords=['protobuf','c++','c','data represent'],  
       author='jj4jj',
       author_email='resc@vip.qq.com',
       license='MIT License',
-      py_modules=['pbdc'],
       packages = find_packages('./'),
       requires = ['Jinja2'],
       include_package_data = True,
-      package_data = {'./':['templates/pb2.proto']},
-      data_files = ['templates/pb2.proto'],
       url='https://github.com/jj4jj/pbdc',
 )
 
